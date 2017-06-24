d9c6de018159c6e7ebdae05dc1a66ac555542ba7
Pradip Caulagi <caulagi@gmail.com>
2017-06-24 13:21:03 +0200

    Use git submodules which makes it easier to test

--- python/Makefile
+++
@@ -1,9 +0,0 @@
-.PHONY: server clean
-
-server:
-	python server/server.py
-
-clean:
-	find . -type f -name '*.py[co]' -delete
-	find . -type d -name '__pycache__' -delete
-	rm -rf .coverage .cache
--- python/server/const.py
+++
@@ -1,8 +0,0 @@
-PG_USERNAME = 'postgres'
-PG_DATABASE = 'rent-slogan'
-PG_PASSWORD = '1234'
-
-
-def connection_url():
-    return 'postgresql://{}:{}@localhost/{}'.format(PG_USERNAME, PG_PASSWORD,
-                                                    PG_DATABASE)
